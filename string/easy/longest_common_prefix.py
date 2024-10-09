cc93cec03c3cd933b4a2f6370c5293ee6ff1c2a1
Mohit Kumar <mohitkumar@Mohits-MacBook-Air.local>
2024-10-09 14:47:10 +0530

    formatted comments in longest common prefix

--- string/easy/longest_common_prefix.py
+++ string/easy/longest_common_prefix.py
@@ -6,7 +6,9 @@ class Solution:
         cmn = strs[0]
         #loop through all other strings in the list
         for i in strs[1:]:
-            #if common prefix is not present in the string, reduce length of prefix by 1 and then check again. Do it till the prefix is found or reduced to empty string
+            #if common prefix is not present in the string, 
+            # reduce length of prefix by 1 and then check again. 
+            # Do it till the prefix is found or reduced to empty string
             while i.find(cmn) != 0:
                 cmn = cmn[:-1]
             if len(cmn) == 0:
